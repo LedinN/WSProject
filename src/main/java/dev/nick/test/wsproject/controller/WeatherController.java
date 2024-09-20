cd7c52e1e423b86a594c067b95a7529d9bd31582
LedinN <nick.ledin@hotmail.com>
2024-09-20 18:54:16 +0200

    add: move api calls to separate class

--- src/main/java/dev/nick/test/wsproject/controller/WeatherController.java
+++ src/main/java/dev/nick/test/wsproject/controller/WeatherController.java
@@ -1,42 +1,29 @@
 package dev.nick.test.wsproject.controller;
 
 import dev.nick.test.wsproject.model.WeatherData;
-import dev.nick.test.wsproject.service.ApiService;
+import dev.nick.test.wsproject.service.WeatherService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
 
-    private final WebClient weatherWebClient;
-    private final ApiService apiService;
+    private final WeatherService weatherService;
 
-    public WeatherController(WebClient.Builder webClient, ApiService apiService){
-        this.weatherWebClient = webClient
-                .baseUrl("https://api.tomorrow.io/v4/weather/realtime")
-                .build();
-        this.apiService = apiService;
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
     }
 
     @GetMapping("/{location}")
-    public Mono<ResponseEntity<WeatherData>> getWeather(@PathVariable String location){
-
-        String apiKey = apiService.getApiKey();
-
-        return weatherWebClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .queryParam("location", location)
-                        .queryParam("apikey", apiKey)
-                        .build())
-                .retrieve()
-                .bodyToMono(WeatherData.class)
+    public Mono<ResponseEntity<WeatherData>> getWeather(@PathVariable String location) {
+        return weatherService.getCurrentWeather(location)
                 .map(ResponseEntity::ok);
+
     }
 
 }
